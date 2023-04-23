ce98f18c1795cbdbad4704e809c0326bd9e82884
Silver Horcrux <hltan8495@gmail.com>
2023-04-23 13:24:45 +0700

    F

--- src/hooks/useSubdomain.ts
+++ src/hooks/useSubdomain.ts
@@ -4,7 +4,8 @@ export default function useSubdomain(): [string | undefined] {
   const [subdomain] = React.useState(() => {
     try {
       const parts = window?.location?.hostname?.split('.');
-      return parts.length > 2 ? parts[0] : '';
+      // return parts.length > 2 ? parts[0] : '';
+      return 'blockbuster';
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (err: any) {
       console.error(err.message);
